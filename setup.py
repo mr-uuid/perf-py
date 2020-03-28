fdc6beaf5298e7b55d9fbcfb5d67874a3c2caa18
Omar Eid <oeid@cognitivescale.com>
2020-03-27 21:14:10 -0500

    Setting proper content type for README

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     url='https://github.com/mr-uuid',
     description='Understanding performance characteristics of common python constructs',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
